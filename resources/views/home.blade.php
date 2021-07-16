5c529baeb6429e1950b1eef5611f0425ca0f5bea
Salahuddin <sholahudinbest@gmail.com>
2021-07-16 22:09:06 +0800

    Change image in homepage
    Change image in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -103,7 +103,7 @@
                         <p>Cinta itu seperti batik yang dibuat dari banyak warna emosional, itu adalah kain yang pola dan kecerahannya mungkin berbeda-beda</p>
                     </div>
                     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
-                        <img src="{{asset('img')}}/img-home.jpg" class="rounded img-fluid d-block mx-auto" alt="First Vector Graphic">
+                        <img src="{{asset('img')}}/bg-login.jpeg" class="rounded img-fluid d-block mx-auto" alt="First Vector Graphic">
                     </div>
                 </div>
             </div>
